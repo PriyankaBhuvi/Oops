a851fe4e2a2551f2302292a7df3f815630dfb0ed
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:45:14 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+package com.vstl.oops;
+
+public class Car {
+	
+	//Single Inheritance :
+	
+	public void getStartCar () {
+		System.out.println("car----start");
+	}		
+	
+   public void getStopCar  ()  {
+	   System.out.println("car-----stop");
+   }   
+   
+	public void getrefuelCar () {
+		System.out.println("car----refuel");
+   }
+	
+	}
+
+

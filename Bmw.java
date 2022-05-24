5a6b35e2617c3081ebd4b829432bf9e8aef04dcd
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:44:17 +0530

    Add files via upload

---
+++ Bmw.java
@@ -0,0 +1,19 @@
+package com.vstl.oops;
+
+public class Bmw extends Car {
+	 
+	public void Start() {
+		System.out.println("Bmw -----Start");
+	}
+	
+	public void Safety()  {
+		System.out.println("Bmw-----Safety");
+		
+	}
+}
+
+
+     
+	
+
+

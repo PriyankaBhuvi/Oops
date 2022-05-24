657eccaa8e8d633329804b6b770943eb01c3a105
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:49:50 +0530

    Add files via upload

---
+++ USBank.java
@@ -0,0 +1,17 @@
+package com.vstl.oops;
+
+public class USBank {
+	
+	//Multilevel Inheritance Example :
+	
+	 public String strUSBankLoan = "EducationlLoan";
+	 public String strUSBankAnotherLoan="finance Laon";
+	 
+	 public void getUSBankAnLoan () {
+		 System.out.println("USBank  loan : "+strUSBankLoan);
+	 }
+	 
+    public void getUSBankGivenLoan() {
+    	System.out.println("USBank Loan : "+strUSBankAnotherLoan);
+    }
+}

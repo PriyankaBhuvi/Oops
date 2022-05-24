eea9f901ab710d343fd8d498bf718d280210797a
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:46:04 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,17 @@
+package com.vstl.oops;
+
+public class Company {
+	
+	
+	public String strCompanyName="Infosys";
+	int intTotalBranch=40;
+	 
+	
+	public void getCompanyName() {
+		System.out.println("My company Name is : "+strCompanyName);
+		
+	}
+   public void getCompanyTotalBranch() {
+	   System.out.println("Company branch  : "+intTotalBranch);
+   }
+}

581c2929997a62f6f806794da76a577ca9dc948a
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:47:37 +0530

    Add files via upload

---
+++ MarutiSWiftCar.java
@@ -0,0 +1,18 @@
+package com.vstl.oops;
+
+public class MarutiSWiftCar {
+	/*Hierarchical Inheritance :-When two or more classes inherits a single class, 
+	it is known as hierarchical inheritance*/
+	
+	public int intMarutiSWiftCarWheel=4;
+	public String strMarutiSWiftCarStart="Start";
+	public String strMarutiSWiftCarStop="stop";
+	public String strMarutiSWiftCarRefuel="Refuel";
+	public String strMarutiSWiftCarEngine="Engine";
+	
+	public void getAllMarutiSWiftCarDetails() {
+		
+		
+	}
+
+}

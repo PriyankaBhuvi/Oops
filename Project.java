fe6e2cfc7efd0ff6c7a639fa0a8d7314851f38fc
PriyankaBhuvi <105700636+PriyankaBhuvi@users.noreply.github.com>
2022-05-24 20:48:04 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,11 @@
+package com.vstl.oops;
+
+public interface Project {
+	
+	String strProjectName ="Square";
+	int intTakenDaysToComplteTheProject =30;
+	
+	public static void getProjectDetail() {
+
+  }
+}
